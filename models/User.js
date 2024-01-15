3bd07d320acaf896ecd6f4e343449c386571c122
Keon24 <keonpowell95@gmail.com>
2024-01-15 10:11:22 -0800

    configured user name and password

--- models/User.js
+++ models/User.js
@@ -1,18 +1,34 @@
 
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 const UserSchema = new mongoose.Schema(
     {
-      name: {
-      type: string,
-      required: ["Please enter your name"],
-      trim: true
+      firstName: {
+      type: String,
+      required: "Please enter your first name",
+      trim: true,
+      minLength: 2,
+      maxLength: 20,
+      },
+      lastName: {
+      type: String,
+      required: "Please enter your last name",
+      trim: true,
+      minLength: 2,
+      maxLength: 20
       },
       email: {
-        type:string,
-        required: ["Please enter your email"],
+        type: String,
+        required: "Please enter your email",
         trim: true,
         unique: true 
       },
+      password: {
+        type: String,
+        required: "Please enter a password",
+        minLength: 5,
+      },
       
     }
-)
+);
+
+export default mongoose.model("User", UserSchema);
